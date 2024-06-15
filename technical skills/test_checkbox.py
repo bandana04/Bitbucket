13c19bc8b89e9f74ddcbd336f80b63da65890634
Bandana Tigela Limbu <161959084+bandana04@users.noreply.github.com>
2024-06-15 13:35:23 +0545

    Update test_checkbox.py

--- technical skills/test_checkbox.py
+++ technical skills/test_checkbox.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 def setup_driver():
-    service = Service('C:/Users/Acer/Downloads/chromedriver-win64/chromedriver.exe')  # specify the full path to chromedriver.exe
+    service = Service('C:/Users/Acer/Downloads/chromedriver-win64/chromedriver.exe')
     driver = webdriver.Chrome(service=service)
-    driver.implicitly_wait(10)  # implicit wait
+    driver.implicitly_wait(10)
     return driver
 
 def teardown_driver(driver):
